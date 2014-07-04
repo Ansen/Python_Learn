f1e218af57af89260cd5c76217f7e2ce21f84b24
Ansen <shen.an89@gmail.com>
2014-07-04 18:24:13 +0800

    add
    add

---
+++ Hex2Ascii.py
@@ -0,0 +1,19 @@
+''' Hex convert to ascii'''
+import binascii
+
+def str2list(list):
+    for i in range(0,len(list)-1,2):
+        print '%s%s ' %(list[i], list[i+1]),
+        
+
+def Hex2ascii(char):
+    value = []
+    for i in range(len(char)):
+        b = binascii.b2a_hex(char[i])
+        b = chr(int(b))
+        value.append(b)
+    return value
+
+a = '8A 67 5F 6C'
+a = ''.join(a.split())
+str2list(Hex2ascii(list(a)))
